3ca446a45c3ca0691fead2629d90df2dfd54932a
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-11-13 14:52:48 +0100

    created and finished repository file, and only created service.java. Both of 'Monitoreo consumo...'

---
+++ src/main/java/org/factoriaf5/powermate/repositories/ConsRecordRepository.java
@@ -0,0 +1,8 @@
+package org.factoriaf5.PowerMate.powermate.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsRecordRepository extends JpaRepository<PowerMate, Long> {
+
+
+}
---
+++ src/main/java/org/factoriaf5/powermate/services/ConsRecodService.java
@@ -0,0 +1,11 @@
+package main.java.org.factoriaf5.powermate.services;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsRecodService {
+    
+}
