cb534aa2209289d3736703230696e6313fa3b864
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-14 14:28:47 +0100

    conflictos resueltos

--- src/main/java/org/factoriaf5/powermate/controllers/DeviceController.java
+++ src/main/java/org/factoriaf5/powermate/controllers/DeviceController.java
@@ -1,9 +1,6 @@
 package org.factoriaf5.powermate.controllers;
-
 import java.util.List;
-
 import org.factoriaf5.powermate.models.Device;
-import org.factoriaf5.powermate.services.DeviceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,36 +12,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping(path = "/api")
 public class DeviceController {
-
-    DeviceService service;
-
+    private final DeviceService service;
     @PostMapping(path = "/admin/devices")
-    public ResponseEntity<Device> addDevice(@RequestBody Device device) {
-        return new ResponseEntity<>(service.createDevice(device), HttpStatus.CREATED);
+    public ResponseEntity<Device> addDevice(@RequestBody Device deviceId) {
+        return new ResponseEntity<>(service.createDevice(deviceId), HttpStatus.CREATED);
     }
-
     @GetMapping(path = "/devices")
     public ResponseEntity<List<Device>> getDevice() {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
-
     @PutMapping(path = "/admin/devices/{id}")
-    public ResponseEntity<Device> updateDevice(@RequestParam Device device, Long id) {
-        return new ResponseEntity<>(service.updateDevice(device, id), HttpStatus.OK);
+    public ResponseEntity<Device> updateDevice(@RequestParam Device device, Long deviceId) {
+        return new ResponseEntity<>(service.updateDevice(device, deviceId), HttpStatus.OK);
     }
-
     @PatchMapping(path = "/devices/{id}")
-    public ResponseEntity<Device> updateStatus(@RequestParam Long id, boolean status) {
-        return new ResponseEntity<>(service.updateStatus(id, status), HttpStatus.OK);
+    public ResponseEntity<Device> updateStatus(@RequestParam Long deviceId, Boolean status) {
+        return new ResponseEntity<>(service.updateStatus(deviceId, status), HttpStatus.OK);
     }
-
     @DeleteMapping(path = "/admin/devices/{id}")
-    public ResponseEntity<Void> deleteDevice(@RequestParam Long id) {
-        service.deleteDevice(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Device> deleteDevice(@RequestParam Long deviceId) {
+        return new ResponseEntity<>(service.deleteDevice(deviceId), HttpStatus.OK);
     }
 }
