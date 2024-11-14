02f06300f035e5cc05060dce83c031433bffdb51
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-14 13:03:16 +0100

    Schedule repository

---
+++ src/main/java/org/factoriaf5/powermate/repositories/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package org.factoriaf5.powermate.repositories;
+
+import org.factoriaf5.powermate.models.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    
+}
