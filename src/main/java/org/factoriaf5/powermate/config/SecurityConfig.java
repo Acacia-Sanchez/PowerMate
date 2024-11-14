6b93e5c6fcf8572ce2f040b88b2da4c87aec5336
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-14 15:33:04 +0100

    añadidas configuraciones de seguridad. Tiene bugs. Hay conflictos entre las clases de User y Device. Añadido dataInitializer

---
+++ src/main/java/org/factoriaf5/powermate/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.factoriaf5.powermate.config;
+
+import org.factoriaf5.powermate.services.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(request ->
+        request.requestMatchers("/api/admin/**").hasRole("ADMIN").anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
--- src/main/java/org/factoriaf5/powermate/repositories/UserRepository.java
+++ src/main/java/org/factoriaf5/powermate/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package org.factoriaf5.powermate.repositories;
 
+import java.util.Optional;
+
 import org.factoriaf5.powermate.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
+    public boolean existsByUsername(String username);
 }
