83e302bd0680431fa0cc886e828f157b04d6dd02
abelpriem <abelpriem94@hotmail.com>
2024-11-14 12:29:03 +0100

    modificado endpoints con un naming más intuitivo y personalizado por user-admin

--- src/main/java/org/factoriaf5/powermate/controllers/DeviceController.java
+++ src/main/java/org/factoriaf5/powermate/controllers/DeviceController.java
@@ -1,6 +1,7 @@
 package org.factoriaf5.powermate.controllers;
 
 import java.util.List;
+
 import org.factoriaf5.powermate.models.Device;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,50 +16,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/api")
+@RequestMapping(path = "/api")
 public class DeviceController {
 
     DeviceService service;
 
-    @PostMapping(path = "/admin/add-device")
-    public ResponseEntity<Device> addDevice(@RequestBody Device device){
-
+    @PostMapping(path = "/admin/device")
+    public ResponseEntity<Device> addDevice(@RequestBody Device device) {
         return new ResponseEntity<>(service.createDevice(device), HttpStatus.CREATED);
-        
     }
 
-    @GetMapping(path="/get-device")
-    public ResponseEntity<List<Device>> getDevice(){
-
-        return new ResponseEntity<>(service.getAll(),HttpStatus.OK);
-
+    @GetMapping(path = "/device")
+    public ResponseEntity<List<Device>> getDevice() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
-    @PutMapping(path="admin/update-device/{id}")
-    public ResponseEntity<Device> updateDevice(@RequestParam Long id){
-
-        return new ResponseEntity<>(service.updateDevice(),HttpStatus.OK);
-
-
+    @PutMapping(path = "/admin/device/{id}")
+    public ResponseEntity<Device> updateDevice(@RequestParam Long id) {
+        return new ResponseEntity<>(service.updateDevice(), HttpStatus.OK);
     }
 
-
-    @PatchMapping(path="/update-status/{id}")
-    public ResponseEntity<Device> updateStatus(@RequestParam Long id){
-
-        return new ResponseEntity<>(service.updateStatus(),HttpStatus.OK);
-
-
+    @PatchMapping(path = "/device/{id}")
+    public ResponseEntity<Device> updateStatus(@RequestParam Long id) {
+        return new ResponseEntity<>(service.updateStatus(), HttpStatus.OK);
     }
 
-    @DeleteMapping(path="admin/delete-device/{id}")
-    public ResponseEntity<Device> deleteDevice(@RequestParam Long id){
-
-        return new ResponseEntity<>(service.deleteDevice(),HttpStatus.OK);
-
-
+    @DeleteMapping(path = "/admin/device/{id}")
+    public ResponseEntity<Device> deleteDevice(@RequestParam Long id) {
+        return new ResponseEntity<>(service.deleteDevice(), HttpStatus.OK);
     }
-
 }
-
-
--- src/main/java/org/factoriaf5/powermate/controllers/UserController.java
+++ src/main/java/org/factoriaf5/powermate/controllers/UserController.java
@@ -1,15 +1,22 @@
 package org.factoriaf5.powermate.controllers;
 
+import java.util.List;
+
 import org.factoriaf5.powermate.models.User;
 import org.factoriaf5.powermate.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
@@ -19,30 +26,32 @@ public class UserController {
     }
 
     // 1. Crear un nuevo usuario
-    @PostMapping(path = "/register")
+    @PostMapping(path = "/admin/register")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
     // 2. Obtener un usuario por su ID
-    /* @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        Optional<User> user = userService.getUserById(id);
-        return user.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(null));
-    } */
+    /*
+     * @GetMapping("/users/{id}")
+     * public ResponseEntity<User> getUserById(@PathVariable Long id) {
+     * Optional<User> user = userService.getUserById(id);
+     * return user.map(ResponseEntity::ok)
+     * .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+     * .body(null));
+     * }
+     */
 
     // 3. Obtener todos los usuarios
-    @GetMapping(path = "/getUsers")
+    @GetMapping(path = "/users")
     public ResponseEntity<List<User>> listUsers() {
         List<User> users = userService.listUsers();
         return ResponseEntity.ok(users);
     }
 
     // 4. Actualizar un usuario existente
-    @PutMapping("/updateUser/{id}")
+    @PutMapping("/admin/users/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
         try {
             User updatedUser = userService.updateUser(id, userDetails);
@@ -54,7 +63,7 @@ public class UserController {
     }
 
     // 5. Eliminar un usuario
-    @DeleteMapping("/deleteUser{id}")
+    @DeleteMapping("/admin/users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         if (userService.getUserById(id).isPresent()) {
             userService.deleteUser(id);
@@ -63,5 +72,4 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
-
 }
