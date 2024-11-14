12c5b99d7aec280c03d5d0fadb4a70d466e7252f
Rubén Blanco <r.blanco.3d@gmail.com>
2024-11-14 14:41:24 +0100

    Guardado para añadir MockBean a Dev

--- src/test/java/org/factoriaf5/powermate/controllers/DeviceControllerTest.java
+++ src/test/java/org/factoriaf5/powermate/controllers/DeviceControllerTest.java
@@ -1,11 +1,14 @@
 package org.factoriaf5.powermate.controllers;
 
 import org.factoriaf5.powermate.models.Device;
+import org.factoriaf5.powermate.services.DeviceService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(DeviceController.class)
