dc620e89b2bd15bfcfc16321ff8ed4df2783d327
unknown <sealkevinboyabad@gmail.com>
2024-11-15 11:06:36 -0800

    Creación de la clase ScheduleController

---
+++ src/main/java/org/factoriaf5/powermate/controllers/ScheduleController.java
@@ -0,0 +1,11 @@
+package org.factoriaf5.powermate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/schedule/device")
+public class ScheduleController {
+    
+    
+}
