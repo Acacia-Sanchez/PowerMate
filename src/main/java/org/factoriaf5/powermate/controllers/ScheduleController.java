2c04d7df14b1bca851531749c9ad61e035ee2eb1
unknown <sealkevinboyabad@gmail.com>
2024-11-15 11:18:29 -0800

    Añadido endpoint GET by deviceID

--- src/main/java/org/factoriaf5/powermate/controllers/ScheduleController.java
+++ src/main/java/org/factoriaf5/powermate/controllers/ScheduleController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,13 +46,17 @@ public class ScheduleController {
         scheduleService.deleteSchedule(scheduleId);
         return ResponseEntity.noContent().build();
     }
-    
+
     @GetMapping
     public ResponseEntity<List<Schedule>> getAllSchedules(@RequestParam Long deviceId) {
         List<Schedule> schedules = scheduleService.getAllSchedules(deviceId);
         return ResponseEntity.ok(schedules);
     }
 
-
+    @GetMapping("/{deviceId}/check-status")
+    public ResponseEntity<String> checkDeviceStatus(@PathVariable Long deviceId) {
+        String statusMessage = scheduleService.checkDeviceStatus(deviceId);
+        return ResponseEntity.ok(statusMessage);
+    }
 
 }
