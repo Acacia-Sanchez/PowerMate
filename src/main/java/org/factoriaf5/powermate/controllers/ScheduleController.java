8d2e7134cfb10f39ee8e1ff625cb40b0262dcd13
unknown <sealkevinboyabad@gmail.com>
2024-11-15 11:16:56 -0800

    Añadido endpoint GET all

--- src/main/java/org/factoriaf5/powermate/controllers/ScheduleController.java
+++ src/main/java/org/factoriaf5/powermate/controllers/ScheduleController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,12 @@ public class ScheduleController {
     public ResponseEntity<Void> deleteSchedule(@RequestParam Long scheduleId) {
         scheduleService.deleteSchedule(scheduleId);
         return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<Schedule>> getAllSchedules(@RequestParam Long deviceId) {
+        List<Schedule> schedules = scheduleService.getAllSchedules(deviceId);
+        return ResponseEntity.ok(schedules);
     }
 
 
