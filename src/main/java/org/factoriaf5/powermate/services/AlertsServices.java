a634fb42bb90de5542211669db14964365c4147c
Alejandra <siemi121001@gmail.com>
2024-11-15 12:49:45 +0100

    changes in AlertsServices (insert imports and fix spelling)

--- src/main/java/org/factoriaf5/powermate/services/AlertsServices.java
+++ src/main/java/org/factoriaf5/powermate/services/AlertsServices.java
@@ -1,20 +1,29 @@
 package org.factoriaf5.powermate.services;
 
 import java.util.List;
+//import org.factoriaf5.powermate.repositories.AlertRepository;
 
+//import org.factoriaf5.powermate.controllers.AlertsControllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.factoriaf5.powermate.models.Alerts;
+
+
 
 @Service
-
 public class AlertsServices {
-
 @Autowired
-private AlertRepository alertRepository;
 
-public List<AlertsServices> findByDeviceId( long deviceId) {
-    return alertRepository.findByDeviceId(deviceId);
+private final AlertRepository alertRepository;
 
+public AlertsServices(AlertRepository alertRepository) {
+    this.alertRepository = alertRepository;
+}
+
+public interface AlertRepository extends JpaRepository <AlertsServices, Long> {
+    List<AlertsServices> findByDeviceId(long deviceId);
+    List<AlertsServices> findByThresholdGreaterThan(double threshold);
 }
 
 
@@ -23,20 +32,17 @@ public List<AlertsServices> findByThresholdGreaterThan( double threshold) {
 
 }
 
-
-public AlertsServices createAlert(Long userId, Long deviceId, double threshold) {
-    Alert alert = new Alert();
+public Alerts createAlert(Long userId, Long deviceId, double threshold) {
+    Alerts alert = new Alerts();
     alert.setUserId(userId);
     alert.setDeviceId(deviceId);
     alert.setThreshold(threshold);
     return alertRepository.save(alert);
-
 }
 
 public void deleteAlert(Long alertId) {
     alertRepository.deleteById(alertId);
 }
-
 
 public boolean checkAlert(Long deviceId, double currentConsumption) {
     List<Alert> alerts = alertRepository.findByDeviceId(deviceId);
@@ -49,14 +55,11 @@ public boolean checkAlert(Long deviceId, double currentConsumption) {
     return false;
 
 }
+
 public AlertsServices updateAlert(Long alertId, double threshold) {
     AlertsServices alert = alertRepository.findById(alertId).orElseThrow();
     alert.setThreshold(threshold);
     return alertRepository.save(alert);
-
-
-
-
 
 }
 }
