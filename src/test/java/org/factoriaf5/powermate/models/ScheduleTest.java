1d27aa420055a8bd96a7ac9acadb4eebb16cef4c
mercyluz <mercyluz@hotmail.com>
2024-11-21 12:33:23 +0100

    test de dto y service schedule

--- src/test/java/org/factoriaf5/powermate/models/ScheduleTest.java
+++ src/test/java/org/factoriaf5/powermate/models/ScheduleTest.java
@@ -25,7 +25,7 @@ class ScheduleTest {
         // Creamos un Schedule con un mock de Device y un rango de tiempo
         LocalDateTime startTime = LocalDateTime.of(2024, 11, 15, 8, 0);  // 15 de noviembre de 2024, 08:00 AM
         LocalDateTime endTime = LocalDateTime.of(2024, 11, 15, 18, 0);    // 15 de noviembre de 2024, 06:00 PM
-        schedule = new Schedule(mockDevice, startTime, endTime);
+        schedule = new Schedule(null, mockDevice, startTime, endTime);
     }
 
     @Test
